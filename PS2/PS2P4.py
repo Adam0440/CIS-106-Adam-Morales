e071444377217a268c52ace379c3b514da180391
Adam0440 <160086943+Adam0440@users.noreply.github.com>
2024-02-29 01:21:57 -0600

    Add files via upload

---
+++ PS2/PS2P4.py
@@ -0,0 +1,6 @@
+print ("Insert Name")
+name = str(input())
+print ("Insert credits taken")
+credits = float(input())
+str(input(credits*250 +100))
+print ("The tuition for " + name + " is $" + str(credits*250 +100))
