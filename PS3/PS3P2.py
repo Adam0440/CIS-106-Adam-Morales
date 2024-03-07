c1f420cd6b368610b984d73c1e1ef0923be17851
Adam0440 <160086943+Adam0440@users.noreply.github.com>
2024-03-07 12:20:58 -0600

    Add files via upload

---
+++ PS3/PS3P2.py
@@ -0,0 +1,7 @@
+print("Insert last name")
+name = str(input())
+print("Insert midterm points")
+midterm = float(input())
+print("Insert final points")
+final = float(input())
+print("Total points for "+ name+ " will be " + str(midterm + final))
