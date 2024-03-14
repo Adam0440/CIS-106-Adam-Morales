2fa0cab7f48670d609612f884157c42591157fb9
Adam0440 <160086943+Adam0440@users.noreply.github.com>
2024-03-14 13:47:56 -0500

    Add files via upload

---
+++ PS4/PS4P4.py
@@ -0,0 +1,4 @@
+name = input("Enter your name:" )
+steps = float(input(name + ", how many steps have you taken today? "))
+calories = steps * 0.25
+print(name + ", you have burned, ", calories, "calories today.")
