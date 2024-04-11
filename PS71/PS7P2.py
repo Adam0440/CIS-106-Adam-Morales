75438cfbfccff87c889b46b2e0c4dbb581428de7
Adam0440 <160086943+Adam0440@users.noreply.github.com>
2024-04-11 13:18:57 -0500

    Add files via upload

---
+++ PS71/PS7P2.py
@@ -0,0 +1,6 @@
+start = float(input("Enter start value: "))
+increment=float(input("Enter increment value: "))
+end = float(input("Enter end value: "))
+while start<=end:
+  print(start)
+  start=start+increment
