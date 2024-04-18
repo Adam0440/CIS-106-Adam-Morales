49953d279dbc651d48a16b43594f42f376977636
Adam0440 <160086943+Adam0440@users.noreply.github.com>
2024-04-18 12:34:06 -0500

    Add files via upload

---
+++ PS8/PS8P2 (1).py
@@ -0,0 +1,5 @@
+start = int(input("Enter start number"))
+stop = int(input("Enter stop number: "))
+increment = int(input("Enter increment number: "))
+for i in range(start, stop, increment):
+  print(i)
